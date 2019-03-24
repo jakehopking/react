d799bd918d20bad310858af76889bcf63bbf6b15
Jake Hopking <jake@hopking.io>
2019-03-24 13:25:58 +0000

    Refactor app to functional

--- mrr/hooks-simple/src/components/App.js
+++ mrr/hooks-simple/src/components/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-  state = { resource: 'posts' }
-  render() {
-    return (
-      <div className="ui container">
-        <div className="buttons">
-          <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
-          <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>
-        </div>
-        {this.state.resource}
+const App = () => {
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div className="ui container">
+      <div className="buttons">
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
       </div>
-    );
-  }
+      {resource}
+    </div>
+  );
 }
 
 export default App;
