f961fe5de902e1c8e92f0a7c80a6bc90cb6d67e8
Jake Hopking <jake@hopking.io>
2019-04-24 14:20:55 +0100

    finish useless example of axios

--- tcrg/concepts/http/src/index.js
+++ tcrg/concepts/http/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/';
 
 axios.interceptors.request.use(request => {
   console.log(request);
