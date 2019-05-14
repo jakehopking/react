dba49f5f598c8a220fa14ff2fdd2f6b7f3dbc2eb
Jake Hopking <jake@hopking.io>
2019-05-14 17:44:54 +0100

    add route component

--- tcrg/concepts/routing/src/containers/Blog/Blog.js
+++ tcrg/concepts/routing/src/containers/Blog/Blog.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import axios from "../../axios";
 import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
+
 import "./Blog.css";
 
 class Blog extends Component {
@@ -18,7 +20,9 @@ class Blog extends Component {
             </li>
           </ul>
         </header>
-        <Route path="/" exact render={() => <Posts />} />
+        {/* <Route path="/" exact render={() => <Posts />} /> */}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
 
         {/* <section>
           <FullPost id={this.state.selectedPostId} />
