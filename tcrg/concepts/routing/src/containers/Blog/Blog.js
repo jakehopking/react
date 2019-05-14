5a26c86803772d40cfb9429d702a690c09a45e17
Jake Hopking <jake@hopking.io>
2019-05-14 17:40:47 +0100

    Start routing

---
+++ tcrg/concepts/routing/src/containers/Blog/Blog.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import axios from "../../axios";
+import Posts from "./Posts/Posts";
+import "./Blog.css";
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/new-post">New Post</a>
+            </li>
+          </ul>
+        </header>
+        <Route path="/" exact render={() => <Posts />} />
+
+        {/* <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+            <NewPost />
+        </section> */}
+      </div>
+    );
+  }
+}
+
+export default Blog;
