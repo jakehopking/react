c836167d6e6764440e3b261397d392117bbe5491
Jake Hopking <jake@hopking.io>
2019-05-15 07:45:24 +0100

    Continue routing

--- tcrg/concepts/routing/src/containers/Blog/Blog.js
+++ tcrg/concepts/routing/src/containers/Blog/Blog.js
@@ -1,28 +1,43 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import axios from "../../axios";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
 
 import "./Blog.css";
 
 class Blog extends Component {
+  
   render() {
+    console.log(this.props);
     return (
       <div className="Blog">
         <header>
           <ul>
             <li>
-              <a href="/">Home</a>
+              <NavLink 
+                to="/"
+                activeClassName="active--home" 
+                activeStyle={{
+                  textDecoration: 'line-through'
+                }}
+                exact>
+                  Home</NavLink>
             </li>
             <li>
-              <a href="/new-post">New Post</a>
+              <NavLink to={{
+                pathname: '/new-post', // by default absolute
+                // pathname: this.props.match.url + '/new-post', // builds relative
+                hash: '#someanchor',
+              }}>New Post</NavLink>
             </li>
           </ul>
         </header>
         {/* <Route path="/" exact render={() => <Posts />} /> */}
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" exact component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
 
         {/* <section>
           <FullPost id={this.state.selectedPostId} />
